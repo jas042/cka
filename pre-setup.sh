286221ada75703da4694f52c52ec7dbd8577d18d
James <james.assis@gmail.com>
2023-09-09 07:10:20 -0300

    Add files via upload

---
+++ pre-setup.sh
@@ -0,0 +1,6 @@
+source <(kubectl completion bash) # set up autocomplete in bash into the current shell, bash-completion package should be installed first.
+echo "source <(kubectl completion bash)" >> ~/.bashrc # add autocomplete permanently to your bash shell.
+alias k=kubectl
+complete -o default -F __start_kubectl k
+export do="--dry-run=client -o yaml"    # k create deploy nginx --image=nginx $do
+export now="--force --grace-period 0"   # k delete pod x $now
